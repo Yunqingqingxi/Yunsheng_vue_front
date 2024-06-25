0e2df4330404cbf80de6bf9c684f519ac62373b9
Niko <y111626395@outlook.com>
2024-06-26 00:27:59 +0800

    引入开发，生产，测试环境
    Signed-off-by: Niko <y111626395@outlook.com>

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,17 @@ export default defineConfig({
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 8000,
+        open: true,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, '')
+            }
+        }
     }
 })
